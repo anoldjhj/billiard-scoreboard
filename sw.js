b51d1f848a586459b9ab9779205c2b4892eb9ff9
anoldjhj <anoldjhj@gmail.com>
2026-06-06 23:54:55 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "billiards-scoreboard-v196";
+const CACHE_NAME = "billiards-scoreboard-v197";
 const CORE_ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=20260606-96",
-  "./app.js?v=20260606-96",
+  "./styles.css?v=20260606-97",
+  "./app.js?v=20260606-97",
   "./manifest.json",
   "./icons/play-logo.svg",
   "./icons/icon-192.png",
